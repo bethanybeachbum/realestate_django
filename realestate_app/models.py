069bdaeca49e4df43215408a62168a908fa66d7c
Mark Brown <allenmarkbrown@gmail.com>
2023-04-11 14:26:11 -0400

    2nd Commit - models update

--- realestate_app/models.py
+++ realestate_app/models.py
@@ -4,26 +4,65 @@ from django.db import models
 
 class Contract(models.Model):
     contract_num = models.BigIntegerField()
-    seller = models.CharField(max_length=30)
-    buyer = models.CharField(max_length=30)
-    listingAgent = models.CharField(max_length=30)
-    buyingAgent = models.CharField(max_length=30) 
-    price = models.PositiveBigIntegerField()
+    seller = models.CharField(max_length=30, help_text="Enter Seller's Name")
+    buyer = models.CharField(max_length=30, help_text="Enter Buyer's Name")
+    listingAgent = models.CharField(max_length=30, help_text="Enter Listing Agent")
+    buyingAgent = models.CharField(max_length=30, help_text="Enter Buying Agent")
+    price = models.PositiveBigIntegerField(help_text="Enter Price")
+    propertyAddress = models.CharField(max_length=30, help_text="Enter Property Addresss")
+    contractPDF = models.FileField(upload_to='', help_text="Attach Contract")
+    contractDate = models.DateField(auto_now_add=True, help_text="Enter Date of Contract")
+    mortgage_or_cash = models.CharField(max_length=30, help_text="Enter Mortgage or Cash")
+    mortgageAmount = models.PositiveBigIntegerField( help_text= "Enter Mortgage Amount")
+    escrowAmount = models.PositiveBigIntegerField(help_text= "Enter Escrow Amount")
+    closedYesNo = models.CharField(max_length=30, help_text= "Has the contract closed? Enter 'Yes' or No' " )
+    comments = models.TextField(help_text = "Enter pertinent information")
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.my_field_name
+class Transaction(models.Model):
+    contract_num = models.BigIntegerField()
+    closeDate = models.DateField(auto_now_add=True)
     propertyAddress = models.CharField(max_length=30)
-    contractPDF = models.FileField(upload_to='')
-    contractDate = models.DateField(auto_now_add=True)
-    mortgage_or_cash = models.CharField(max_length=30)
-    mortgageAmount =models.PositiveBigIntegerField()
-    escrowAmount = models.PositiveBigIntegerField()
-    closedYesNo = models.CharField(max_length=30)
+    closingDocumentsPDF = models.FileField(upload_to='')
     comments = models.TextField()
 
-class ContractPartners(models.Model):
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.my_field_name
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)  
+    title = models.CharField(max_length=30)
+    company = models.CharField(max_length=30)
+    email =  models.EmailField(max_length=254)
+    phone = models.CharField(max_length=30)
+    street = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    zip = models.CharField(max_length=10)
+    role = models.CharField(max_length=10)
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.my_field_name
+
+class ContractActions(models.Model):
     contract_num = models.BigIntegerField()
-    listingAgentCompany = models.CharField(max_length=30)
-    sellingAgentCompany = models.CharField(max_length=30)
-    appraiser = models.CharField(max_length=30)
-    appraisalCompany = models.CharField(max_length=30)
+    action = models.CharField(max_length=30)
+    actionPerson = models.CharField(max_length=30)
+    actionCompany = models.CharField(max_length=30)
+    actionAction = models.CharField(max_length=30)
+    actionFee = models.CharField(max_length=30)
+    actionDueDate = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+            """String for representing the MyModelName object (in Admin site etc.)."""
+            return self.my_field_name
+
+"""
     inspector = models.CharField(max_length=30)
     inspectionCompany = models.CharField(max_length=30)
     surveyor = models.CharField(max_length=30)
@@ -51,23 +90,4 @@ class ContractPartners(models.Model):
     other2_person = models.CharField(max_length=30)
     other2_Company = models.CharField(max_length=30)
 
-
-
-
-class Transaction(models.Model):
-    contract_num = models.BigIntegerField()
-    closeDate = models.DateField(auto_now_add=True)
-    propertyAddress = models.CharField(max_length=30)
-    closingDocumentsPDF = models.FileField(upload_to='')
-    comments = models.TextField()
-
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)  
-    title = models.CharField(max_length=30)
-    company = models.CharField(max_length=30)
-    email =  models.EmailField(max_length=254)
-    phone = models.CharField(max_length=30)
-    street = models.CharField(max_length=30)
-    city = models.CharField(max_length=30)
-    zip = models.CharField(max_length=10)
+    """
